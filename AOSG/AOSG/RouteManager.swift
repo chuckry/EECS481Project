d78617ffcf8957039f1ef6b7a6f4f1293629dff0
Chuckry Vengadam <chuckry@umich.edu>
2016-10-18 20:23:05 -0400

    Initialize Route Manager

--- AOSG/AOSG/RouteManager.swift
+++ AOSG/AOSG/RouteManager.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import CoreLocation
+import UIKit
+
+class RouteManager {
+    let route: NavigationPath
+    let northVector = Vector2(0, 1)
+    var currentVector: Vector2
+    
+    var previousStep: CGPoint
+    var currentStep: CGPoint
+    
+    init(path: NavigationPath) {
+        self.route = path
+        let startPoint = CGPoint(x: self.route.currentStep().goal.coordinate.longitude, y: self.route.currentStep().goal.coordinate.latitude)
+        self.route.nextStep()
+        let nextPoint = CGPoint(x: self.route.currentStep().goal.coordinate.longitude, y: self.route.currentStep().goal.coordinate.latitude)
+        self.currentVector = self.getVector(start: startPoint, end: nextPoint)
+    }
+    
+    func getVector(start: CGPoint, end: CGPoint) -> Vector2 {
+        return Vector2(Float(currentStep.x) - Float(previousStep.x), Float(currentStep.y) - Float(previousStep.y))
+    }
+    
+    func getAngle(userHeading: Double) -> Double {
+        
+        return 0
+    }
+}
