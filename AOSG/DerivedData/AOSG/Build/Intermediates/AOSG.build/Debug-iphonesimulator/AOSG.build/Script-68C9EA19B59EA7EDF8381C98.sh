5ea0825d4b879e8b85b19088d7ad8d956801f15c
cgluck <gluckca@umich.edu>
2016-12-06 09:31:00 -0500

    delete derived data

--- AOSG/DerivedData/AOSG/Build/Intermediates/AOSG.build/Debug-iphonesimulator/AOSG.build/Script-68C9EA19B59EA7EDF8381C98.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-AOSG/Pods-AOSG-frameworks.sh"
-
--- AOSG/DerivedData/AOSG/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-AOSG.build/Objects-normal/arm64/Pods-AOSG-dummy.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/carolinegluck/EECS481Project/AOSG/Pods/Target\ Support\ Files/Pods-AOSG/Pods-AOSG-dummy.m
