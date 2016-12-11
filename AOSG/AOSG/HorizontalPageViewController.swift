10858d5a561ad266c060cb181b53e833a238bbbd
gapoorva <gapoorva@umich.edu>
2016-12-11 18:47:52 -0500

    Dumb mistake

--- AOSG/AOSG/HorizontalPageViewController.swift
+++ AOSG/AOSG/HorizontalPageViewController.swift
@@ -49,7 +49,8 @@ class HorizontalPageViewController: UIPageViewController {
     }
     
     public func moveToMainScreen() {
-        let mainViewController = orderedViewControllers[1] as! MainViewController
+        let vertical = orderedViewControllers[1] as! VerticalPageViewController
+        let mainViewController = vertical.orderedViewControllers[1] as! MainViewController
         mainViewController.spinner.startAnimating()
         setViewControllers([orderedViewControllers[1]], direction: .reverse, animated: true, completion: nil)
     }
